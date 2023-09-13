29450b07bb75aac9315e79ff532366e021a85c02
Neatartist <davk0810@gmail.com>
2023-09-13 11:00:42 -0400

    small start to mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const ADD_PROFILE = gql`
+  mutation addProfile($name: String!) {
+    addProfile(name: $name) {
+      _id
+      name
+      email
+    }
+  }
+`;
+
