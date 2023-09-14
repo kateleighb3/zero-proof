e3910765d837f505e71177f5f89526e5309c093e
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-09-13 22:10:19 -0400

    Added mutations and queries for location to client

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,26 +5,59 @@ export const QUERY_PROFILES = gql`
     profiles {
       _id
       name
-      comments {
-        _id
-        commentText
-        createdAt
-        username
+      locations
     }
   }
 `;
-
 
 export const QUERY_SINGLE_PROFILE = gql`
   query singleProfile($profileId: ID!) {
     profile(profileId: $profileId) {
       _id
       name
-      comments {
-        _id
-        commentText
-        createdAt
-        username
+      locations
     }
   }
 `;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      name
+      locations
+      favorites
+    }
+  }
+`;
+
+export const QUERY_LOCATIONS = gql`
+  query allLocations {
+    locations {
+      _id
+      createdAt
+      name
+      lat
+      lng
+      photo_ref
+      description
+      username
+    }
+  }
+`;
+
+export const QUERY_SINGLE_LOCATION = gql`
+  query singleLocation($locationId: ID!) {
+    location(locationId: $locationId) {
+      _id
+      createdAt
+      name
+      lat
+      lng
+      photo_ref
+      description
+      username
+      comments
+    }
+  }
+`
